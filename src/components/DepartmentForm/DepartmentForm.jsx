9876f30fd02b28e3e090ac7a94b2278b693b570c
Cancu <juancruz.cancu@gmail.com>
2021-11-25 13:16:51 -0300

    add department finished

--- src/components/DepartmentForm/DepartmentForm.jsx
+++ src/components/DepartmentForm/DepartmentForm.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { CirclePicker } from "react-color";
+import { useRouter } from "next/router";
 
 const DepartmentForm = () => {
+  const router = useRouter();
+
   const [json, setJson] = useState({
     DepartmentName: "",
     DepartmentColor: "",
   });
 
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(json),
+  };
+
   const postData = () => {
-    console.log(json);
+    fetch("http://3.133.98.24:8000/department", requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(() => router.push("/"));
   };
 
   return (
